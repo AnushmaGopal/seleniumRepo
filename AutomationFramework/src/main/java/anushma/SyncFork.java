2505e1c779b06c28387efc5cebcb8b407c109cd6
amalk <amalk@Anushma>
2024-11-18 13:00:42 +0530

    SyncFork class added

---
+++ AutomationFramework/src/main/java/anushma/SyncFork.java
@@ -0,0 +1,11 @@
+package anushma;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SyncFork {
+	@Test
+	public void sync() {
+		Reporter.log("SyncFork class added");
+	}
+}
